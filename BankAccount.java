c156b2a1d196773b3658a97eaa4d2f1f9014395b
theeChad <umufanawapple@gmail.com>
2023-11-03 14:13:14 +0200

    add an output

---
+++ BankAccount.java
@@ -0,0 +1,32 @@
+class SimpleInterest{
+    double principal;
+     double annualRate;
+      int years;
+     
+      void getvalue(double p, double Rate, int time) 
+      {
+        principal = p;
+        annualRate = Rate;
+        years = time;
+      }
+
+     double calculateSimpleInterest(){
+        double Interest = (principal * annualRate * years) / 100.0;
+       return Interest;
+    }
+}
+
+public class BankAccount {
+    public static void main(String[] args) {
+       
+
+        SimpleInterest bobAccount = new SimpleInterest();
+       bobAccount.getvalue(100000, 0.05, 5);
+      double account = bobAccount.calculateSimpleInterest();
+
+        System.out.println("Bob's Simple Interest after 5 years: "+ account);
+    }
+}
+
+
+
